910a3dcd1e23c5eea321f23350c4363ab3340533
sunnyxorange <sunnyorange@gmail.com>
2025-12-24 15:09:30 +0800

    Fix Vercel routes-manifest path by using distDir on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const withNextIntl = createNextIntlPlugin({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.VERCEL ? undefined : 'standalone',
+  distDir: process.env.VERCEL ? 'dist' : undefined,
   reactStrictMode: false,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
